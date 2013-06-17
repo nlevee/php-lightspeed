671163df6ccb3de50c7fcc8a5202cc829a74567f
Nicolas Levée <nlevee@idobs.net>
2013-06-17 16:54:59 +0200

    debug formater

--- src/Lightspeed/Http/Response.php
+++ src/Lightspeed/Http/Response.php
@@ -218,11 +218,11 @@ class Response {
 			}
 		}
 		if (!isset($sConvertBody))
-			$sConvertBody = array_filter(array_map(function($content){
+			$sConvertBody = implode("\n", array_filter(array_map(function($content){
 				if (is_string($content) || (is_object($content) && method_exists($content, '__toString')))
 					return (string) $content;
 				trigger_error("Impossible de transformé le contenu en chaine.", E_USER_WARNING);
-			}, $this->body));
+			}, $this->body)));
 		echo $sConvertBody;
 	}
 
